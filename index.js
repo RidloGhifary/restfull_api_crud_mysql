c5227ac616fdc88a899e53854e5a32419e5c2271
ridho <ridloghfry@gmail.com>
2024-02-05 20:31:09 +0700

    add post feature

--- index.js
+++ index.js
@@ -5,11 +5,29 @@ const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(express.json());
+
 const prisma = new PrismaClient();
 
 app.get("/products", async (req, res) => {
   const products = await prisma.products.findMany();
   res.json(products);
+});
+
+app.post("/products", async (req, res) => {
+  const productsData = req.body;
+
+  const product = await prisma.products.create({
+    data: {
+      name: productsData.name,
+      description: productsData.description,
+      price: productsData.price,
+      image: productsData.image,
+    },
+  });
+
+  res.status(200).json({ message: "Success added data", product });
 });
 
 app.listen(PORT, () => console.log("Server Listening on port: " + PORT));
