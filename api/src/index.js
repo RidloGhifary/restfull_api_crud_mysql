663a2c0e1653a8f8ba3216cc8401d1abc04e807e
ridho <ridloghfry@gmail.com>
2024-02-06 13:31:22 +0700

    add cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const productsRouter = require("./products/products.controller");
